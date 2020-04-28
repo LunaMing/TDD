51ff84a32736e485565c424d901922a80e3a4084
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-04-28 15:40:11 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,14 +1,14 @@
-from selenium import webdriver	  
-import unittest
+from django.test import LiveServerTestCase
+from selenium import webdriver	
 from selenium.webdriver.common.keys import Keys
 import time
 
-class NewVisitorTest(unittest.TestCase): #(1)
+class NewVisitorTest(LiveServerTestCase):
 	
-	def setUp(self):#(3)
+	def setUp(self):
 		self.browser = webdriver.Firefox()
 		
-	def tearDown(self): #(3)
+	def tearDown(self):
 		self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
@@ -16,13 +16,13 @@ class NewVisitorTest(unittest.TestCase): #(1)
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
 		
-	def test_can_start_a_list_and_retrieve_it_later(self): #(2)
+	def test_can_start_a_list_and_retrieve_it_later(self):
 		#Edith has heard about a cool new online to-do app. She goes 
 		#to check out its homepage
-		self.browser.get('http://localhost:8000')#(3)
+		self.browser.get(self.live_server_url)
 
 		#She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title) #(4)
+		self.assertIn('To-Do', self.browser.title) 
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
@@ -66,5 +66,4 @@ class NewVisitorTest(unittest.TestCase): #(1)
 
 		browser.quit()
 
-if __name__ == '__main__': #(6)
-	unittest.main(warnings='ignore')#(7)
+
