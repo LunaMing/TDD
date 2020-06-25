58a3a6524b3bb1d1dbed8b69db44ed47c262745d
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-06-25 14:30:52 +0800

    setting.py 将debug设置为false，同时加入ALLOWED_HOSTS

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'br!nvojgb+axwn6)25554o!57*1((r&b%g0a#^fhh=e#!kddfl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+TEMPLATE_DEBUG = DEBUG
+
+# Needed when DEBUG=False
+ALLOWED_HOSTS = ['47.114.177.38']
 
 
 # Application definition
